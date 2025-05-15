d4af3d10c64d5a0dd9b6a32a84ad82df8a2dd0a4
rustleupchef <rustle.up.chef@gmail.com>
2025-05-14 20:58:38 -0500

    loads projects for window onload in home section of website.

--- src/main/resources/static/home/script.js
+++ src/main/resources/static/home/script.js
@@ -4,3 +4,26 @@ function showTab(tab) {
     }
     window.location.href = tab;
 }
+
+window.onload = function() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/get-projects", true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const projects = JSON.parse(xhr.responseText);
+            const projectList = document.getElementById("project-list");
+            projectList.innerHTML = "";
+            projects.forEach(project => {
+                const li = document.createElement("li");
+                li.innerHTML = "<h3>" + project.name + "</h3><p>" + project.description + "</p>";
+                li.onclick = function() {
+                    window.location.href = "/project/" + project.id;
+                };
+                projectList.appendChild(li);
+            });
+        } else {
+            console.error("Failed to fetch projects:", xhr.statusText);
+        }
+    }
+    xhr.send();
+}

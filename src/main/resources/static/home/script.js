e974daddb0ac8b96b65819c67fee40a31f82639d
rustleupchef <rustle.up.chef@gmail.com>
2025-12-31 23:45:43 -0600

    changed home page

--- src/main/resources/static/home/script.js
+++ src/main/resources/static/home/script.js
@@ -5,22 +5,50 @@ function showTab(tab) {
     window.location.href = tab;
 }
 
+function grabImage(type) {
+    const order = ["java", "py", "js", "cpp", "cs", "c"];
+    const icons = {
+        "c" : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+        "cpp" : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+        "java" : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+        "cs" : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg",
+        "py" : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+        "js" : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+        "default" : "https://cdn-icons-png.flaticon.com/128/9496/9496401.png"};
+    return icons[order[type - 1]] || icons["default"];
+
+}
+
 window.onload = function() {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/get-projects", true);
     xhr.onload = function() {
         if (xhr.status === 200 && xhr.readyState === 4) {
             const projects = JSON.parse(xhr.responseText);
-            console.log(projects);
-            const projectList = document.getElementById("projectList");
+            const projectList = document.getElementById("projects");
             projectList.innerHTML = "";
             projects.forEach(project => {
                 const bar = document.createElement("div");
-                bar.className = "project-bar";
-                bar.innerHTML = "<h3>" + project.name + "</h3><h5>" + project.description + "</h5>";
+                bar.className = "project-box";
                 bar.onclick = function() {
                     window.location.href = "/project?id=" + project.id;
                 };
+
+                const img = document.createElement("img");
+                img.src = grabImage(project.type);
+                bar.appendChild(img);
+
+                const contentDiv = document.createElement("div");
+
+                const title = document.createElement("h3");
+                title.textContent = project.name;
+                contentDiv.appendChild(title);
+                
+                const desc = document.createElement("p");
+                desc.textContent = project.description;
+                contentDiv.appendChild(desc);
+
+                bar.appendChild(contentDiv);
                 projectList.appendChild(bar);
             });
         } else {

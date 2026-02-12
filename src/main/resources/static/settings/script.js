f1aa067858f03a75dd54702a4a05467e3bacd4a4
rustleupchef <rustle.up.chef@gmail.com>
2026-02-11 22:30:37 -0600

    added frontend logic for editing inputs without backend requests

--- src/main/resources/static/settings/script.js
+++ src/main/resources/static/settings/script.js
@@ -8,3 +8,32 @@ function logout() {
     }
     xhr.send();
 }
+
+function edit(name, element) {
+
+    
+    const input = document.getElementById(name);
+    
+    if (input.disabled) {
+        const inputs = document.querySelectorAll('input[type="text"]');
+        inputs.forEach(i => {
+            i.disabled = true;
+            i.style.color = "gray";
+        });
+
+        const buttons = document.querySelectorAll('.edit-button');
+        buttons.forEach(i => {
+            i.innerText = "Edit";
+        });
+
+        input.disabled = false;
+        input.style.color = "white";
+    
+        element.innerText = "Save";
+    } else {
+        input.disabled = true;
+        input.style.color = "gray";
+
+        element.innerText = "Edit";
+    }
+}
